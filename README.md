fd7794a4188ded187c7189f359b166c055e80264
Dan Humphreys <dan@pixelflow.xyz>
2021-08-02 23:41:52 +0100

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # f1-decisions
+
+A tool to download all decision documents from the F1/FIA's decisioning page.
+
+## Description
+
+Written in Python and harnessing the power of BeautifulSoup, this tool is used to download decision documents from the FIA's decisioning page.
+
+## Getting Started
+
+### Dependencies
+
+* `python3`
+* `pip3`
+* BeautifulSoup
+* Loguru
+* Requests
+
+### Installing and using
+
+* `pip3 install -r requirements.txt
+* `python3 scraper.py`
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+## Issues
+
+If you have an issues, please use the GitHub issues page.
---
+++ scraper.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from loguru import logger
+import sys
+
+logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>", level="DEBUG")
+url = "https://www.fia.com/documents/season/season-2021-1108/championships/fia-formula-one-world-championship-14"
+
+# If folder doesn't exist, it will be created. You can change this if you want.
+folder_location = r'documents/'
+if not os.path.exists(folder_location):
+    os.mkdir(folder_location)
+    logger.info(f"{folder_location} didn't exist until created")
+
+response = requests.get(url)
+logger.debug(f"{url} downloaded")
+soup= BeautifulSoup(response.text, "html.parser")  
+
+
+for link in soup.select("a[href$='.pdf']"):
+    race = link['href'].split('/')[-1]
+    race = race.split('-')[0]
+    year = race.split()[0]
+    race_folder = folder_location + year + "/" + race
+    year_folder = folder_location + year
+    if not os.path.exists(year_folder):
+        os.mkdir(year_folder)
+        logger.info(f"{year_folder} didn't exist until created")
+    if not os.path.exists(race_folder):
+        os.mkdir(race_folder)
+        logger.info(f"{race_folder} didn't exist until created")
+    filename = os.path.join(race_folder,link['href'].split('/')[-1])
+    if os.path.isfile(filename):
+        logger.info(f"{filename} exists. Ignoring.")
+    else:
+        with open(filename, 'wb') as f:
+            logger.debug(f"Found {filename} - downloading...")
+            f.write(requests.get(urljoin(url,link['href'])).content)
